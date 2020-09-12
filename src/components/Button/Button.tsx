bc2be1343a9c9e6d8661b9d53d97309f1b83433e
Metruazanca <metruzanca@gmail.com>
2020-09-13 00:57:20 +0200

    style guide WIP

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React, { } from 'react';
+import { Editable } from 'interfaces';
+
+
+interface ButtonProps extends Editable{
+  
+}
+
+export const Button: React.FC<ButtonProps> = () => {
+
+  return (
+    <div>I am Button</div>
+  )
+} 
