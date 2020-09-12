fbd78244872ae7e91fedc9cad10900377313cc5b
Samuele Zanca <samuele.zanca@mia-platform.eu>
2020-09-12 17:21:44 +0200

    Working version

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Bookmark } from './components';
+import { Bookmark } from '../../components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>NightTab v3</h1>
         <Bookmark />
       </header>
     </div>
