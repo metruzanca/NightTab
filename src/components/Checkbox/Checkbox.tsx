9e80674692ad2330328816498ac0174860fc3425
Metruazanca <metruzanca@gmail.com>
2020-09-13 19:09:03 +0200

    Added checkbox Needs to be reworked with form-input

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -4,20 +4,37 @@ import uniqueId from 'lodash.uniqueid'
 import './Checkbox.css'
 import { Disabled } from 'interfaces'
 
-interface CheckboxProps extends Disabled {
+export enum CheckboxType{
+  default = '',
+  line = 'form-input-button-line',
+  ring = 'form-input-button-ring',
+} 
 
+interface CheckboxProps extends Disabled {
+  className?:string
+  type?:CheckboxType
+  hideCheckbox?:boolean
 }
 
-export const Checkbox: React.FC<CheckboxProps> = () => {
-
+export const Checkbox: React.FC<CheckboxProps> = ({
+  className = undefined,
+  type = CheckboxType.default,
+  disabled = false,
+  hideCheckbox = false,
+  children = 'Checkbox',
+}) => {
+  // If btnClass needs to change, replace useState with useMemo
+  const [btnClass] = useState(classNames(
+    'form-input-button', className, type, { "form-input-hide": hideCheckbox }
+  ))
   const [checkboxId] = useState(uniqueId("checkbox-button_"))
 
   return (
-    <div className="form-input-button">
-      <input id={checkboxId} type="checkbox" tabIndex={1}/>
+    <div className={btnClass}>
+      <input id={checkboxId} type="checkbox" tabIndex={1} disabled={disabled}/>
       <label htmlFor={checkboxId} className="mb-0">
         <span className="label-icon"/>
-        Checkbox button
+        {children}
       </label>
     </div>
   )
